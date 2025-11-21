25af5bea20f4c20e4e6ccfcf7dc5fa8f4919232a
angavaielangovan <43002329+angavaielangovan@users.noreply.github.com>
2025-11-21 15:48:05 +0530

    Delete astranov2025-projects directory

--- astranov2025-projects/day2/exceptions/EmailException.py
+++
@@ -1,7 +0,0 @@
-class EmailException(Exception):
-    """Custom exception for email-related errors."""
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-    
--- astranov2025-projects/day2/supplychain/location/models.py
+++
@@ -1,8 +0,0 @@
-class Location:
-    def __init__(self, code,name,latitude,longitude):
-        self.__code = code    
-        self.__name = name
-        self.__latitude = latitude
-        self.__longitude = longitude
-    def show(self):
-        return f"Location[code={self.__code}, name={self.__name}, latitude={self.__latitude}, longitude={self.__longitude}]"    
